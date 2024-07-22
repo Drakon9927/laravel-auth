caa820373d8519bdcd0805cb74d0bc7e7c822251
Antonino <drakon9927@libero.it>
2024-07-22 22:13:19 +0200

    new API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,9 +8,22 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    // Metodo per ottenere tutti i progetti
     public function index()
     {
         $projects = Project::all();
         return response()->json($projects);
+    }
+
+    // Metodo per ottenere un singolo progetto
+    public function show($id)
+    {
+        $project = Project::find($id);
+
+        if (!$project) {
+            return response()->json(['message' => 'Project not found'], 404);
+        }
+
+        return response()->json($project);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
 
+// Ottiene tutti i progetti
 Route::get('/projects', [ProjectController::class, 'index']);
 
+// Ottiene un singolo progetto per ID
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
 
+// Endpoint per ottenere l'utente autenticato, protetto da Sanctum
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
